8dc1b73c84fd2d71fc65cde6ec330713e0e3ec6d
Andrii Zaika <lytiy7404@gmail.com>
2025-04-09 16:49:14 +0300

    remove comments, username replace by email , message of constraints translate to eng

--- src/main/java/ua/shpp/dto/SignInRequest.java
+++ src/main/java/ua/shpp/dto/SignInRequest.java
@@ -1,21 +1,19 @@
 package ua.shpp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-//Ticket Scrum-33
 @Data
-//@Schema(description = "Запит на аутентифікацію")
+@Schema(description = "Sign in request")
 public class SignInRequest {
 
-//    @Schema(description = "Ім'я користувача", example = "User1")
-    @Size(min = 5, max = 50, message = "Ім'я користувача має бути від 5 до 50 символів")
-    @NotBlank(message = "Ім'я користувача не може бути порожнім")
-    private String username;
+    @Size(min = 5, max = 50, message = "email length must be min = 5 and max = 50 characters")
+    @NotBlank(message = "email cannot be blank")
+    private String email;
 
-//    @Schema(description = "Пароль", example = "my_1secret1_password")
-    @Size(min = 8, max = 255, message = "Довжина пароля має бути від 8 до 255 символів")
-    @NotBlank(message = "Пароль не може бути порожнім")
+    @Size(min = 8, max = 255, message = "password length must be from 8 to 255 characters")
+    @NotBlank(message = "password cannot be blank")
     private String password;
 }
