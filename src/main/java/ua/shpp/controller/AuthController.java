9b500884764e5de0523a78709b6fb75363f7f98d
Fenyuk-Tanya <koval.fenjuk@gmail.com>
2025-04-10 11:54:07 +0300

    Add swagger annotation

--- src/main/java/ua/shpp/controller/AuthController.java
+++ src/main/java/ua/shpp/controller/AuthController.java
@@ -1,5 +1,11 @@
 package ua.shpp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,17 +21,30 @@ import ua.shpp.service.AuthenticationService;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
-//@Tag(name = "Аутентифікація")
+@Tag(name = "Authentication", description = "User registration and authorization operations")
 public class AuthController {
     private final AuthenticationService authenticationService;
 
-    //@Operation(summary = "Реєстрація користувача")
+    @Operation(summary = "User registration", description = "Registering a new user with returning a JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful registration",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = JwtAuthenticationResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Incorrect data", content = @Content)
+    })
     @PostMapping("/sign-up")
     public JwtAuthenticationResponse signUp(@RequestBody @Valid SignUpRequest request) {
         return authenticationService.signUp(request);
     }
 
-    //@Operation(summary = "Авторизація користувача")
+
+    @Operation(summary = "User authorization", description = "User login and obtaining a JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful authorization",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = JwtAuthenticationResponse.class))),
+            @ApiResponse(responseCode = "401", description = "Incorrect credentials", content = @Content)
+    })
     @PostMapping("/sign-in")
     public JwtAuthenticationResponse signIn(@RequestBody @Valid SignInRequest request) {
         return authenticationService.signIn(request);
