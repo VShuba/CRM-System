ff21aae5deb45a2a6434317a6d37216afe24782d
Andrew Zaika <lytiy7404@gmail.com>
2025-04-14 23:54:26 +0300

    add BranchRepository

---
+++ src/main/java/ua/shpp/repository/BranchRepository.java
@@ -0,0 +1,9 @@
+package ua.shpp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ua.shpp.entity.BranchEntity;
+
+@Repository
+public interface BranchRepository extends JpaRepository<BranchEntity, Integer> {
+}
