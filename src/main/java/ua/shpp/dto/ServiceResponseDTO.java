9e39a81ed02c8deeadc1dfc6224492992bfbe93f
Vladimir <marvelll1337@gmail.com>
2025-04-17 14:57:50 +0300

    add ex + handle ex + service controller + Swagger

--- src/main/java/ua/shpp/dto/ServiceResponseDTO.java
+++ src/main/java/ua/shpp/dto/ServiceResponseDTO.java
@@ -1,10 +1,23 @@
 package ua.shpp.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "Response with service information")
 public record ServiceResponseDTO(
+
+        @Schema(description = "Service ID", example = "10")
         Long id,
+
+        @Schema(description = "Service name", example = "Pool-dance")
         String name,
+
+        @Schema(description = "Service color in HEX", example = "#AABBCC")
         String color,
+
+        @Schema(description = "Branch name", example = "Central office")
         String branchName,
+
+        @Schema(description = "Room name", example = "Room 3")
         String roomName
 ) {
 }
