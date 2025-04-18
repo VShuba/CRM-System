f4d5306a4064f184f1658a376eb8d838e175e983
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-18 20:25:20 +0200

    {0.0.25} create EmployeeController

---
+++ src/main/java/ua/shpp/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package ua.shpp.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+import ua.shpp.dto.EmployeeRequestDTO;
+import ua.shpp.dto.EmployeeResponseDTO;
+import ua.shpp.entity.EmployeeService;
+
+@Controller
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @PostMapping(path = "/employee", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    private ResponseEntity<EmployeeResponseDTO> addEmployee(@RequestPart MultipartFile avatarImg, @RequestPart EmployeeRequestDTO employeeDTO) {
+        EmployeeResponseDTO employeeResponseDTO = employeeService.createEmployee(avatarImg, employeeDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponseDTO);
+    }
+}
