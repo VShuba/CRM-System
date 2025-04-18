f769ca2f0e9e7d9313cdb8db7b4b4d76e59cfbe9
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-18 20:24:05 +0200

    {0.0.24} apply lombok annotations to EmployeeEntity

--- src/main/java/ua/shpp/entity/EmployeeEntity.java
+++ src/main/java/ua/shpp/entity/EmployeeEntity.java
@@ -1,11 +1,17 @@
 package ua.shpp.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class EmployeeEntity {
 
     @Id
