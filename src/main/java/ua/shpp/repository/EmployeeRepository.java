19b49e089accaa3ea2c1c6939fad2af8cb2f161d
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-18 20:19:57 +0200

    {0.0.23} create EmployeeRepository

---
+++ src/main/java/ua/shpp/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package ua.shpp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.shpp.entity.EmployeeEntity;
+
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Integer> {
+}
