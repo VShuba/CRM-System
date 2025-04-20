b4ccf2b1b79fc2a91249ae1c604675b49b6cf234
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-20 11:07:18 +0200

    {0.0.31} feat(config): add H2ServerConfig to enable TCP access jdbc:h2:tcp://localhost:9092/mem:dbname

---
+++ src/main/java/ua/shpp/configuration/H2ServerConfig.java
@@ -0,0 +1,17 @@
+package ua.shpp.configuration;
+
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2ServerConfig {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
