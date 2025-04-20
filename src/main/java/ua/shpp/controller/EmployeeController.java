45b8a2f04ecdb0fbf76fbc2cb2fe78ed8462a2be
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-20 23:19:55 +0200

    {0.0.42} refactor(controller): update addEmployee method to deserialize employee DTO manually

--- src/main/java/ua/shpp/controller/EmployeeController.java
+++ src/main/java/ua/shpp/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package ua.shpp.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +21,24 @@ import ua.shpp.service.EmployeeService;
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 @Tag(name = "Employees", description = "Operations related to employee")
+@Slf4j
 public class EmployeeController {
     private final EmployeeService employeeService;
+    private final ObjectMapper objectMapper;
 
-    @PostMapping(path = "/employee", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @PostMapping(path = "/employee",
+            consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
     private ResponseEntity<EmployeeResponseDTO> addEmployee(@RequestPart(name = "avatar") MultipartFile avatarImg,
-                                                            @RequestPart(name = "employee") EmployeeRequestDTO employeeDTO) {
-        EmployeeResponseDTO employeeResponseDTO = employeeService.createEmployee(avatarImg, employeeDTO);
+                                                            @RequestPart(name = "employee") String employeeDTOStr) {
+        try {
+            EmployeeRequestDTO employeeRequestDTO = objectMapper.readValue(employeeDTOStr, EmployeeRequestDTO.class);
+            log.info("Deserialized string into EmployeeRequestDTO: {}", employeeRequestDTO.toString());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponseDTO);
+            EmployeeResponseDTO employeeResponseDTO = employeeService.createEmployee(avatarImg, employeeRequestDTO);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponseDTO);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
