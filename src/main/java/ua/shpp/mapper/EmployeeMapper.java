9ab4e168521db404b378c14b628d00e96addb166
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-20 20:20:57 +0200

    {0.0.39} refactor(mapper): adapt employeeEntityToEmployeeResponseDTO to updated EmployeeResponseDTO

--- src/main/java/ua/shpp/mapper/EmployeeMapper.java
+++ src/main/java/ua/shpp/mapper/EmployeeMapper.java
@@ -2,9 +2,14 @@ package ua.shpp.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import ua.shpp.dto.EmployeeRequestDTO;
 import ua.shpp.dto.EmployeeResponseDTO;
 import ua.shpp.entity.EmployeeEntity;
+import ua.shpp.entity.ServiceEntity;
+
+import java.util.List;
+import java.util.Set;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -14,5 +19,14 @@ public interface EmployeeMapper {
     EmployeeEntity EmployeeRequestDTOToEmployeeEntity(EmployeeRequestDTO requestDTO);
 
     @Mapping(target = "base64Avatar", source = "base64Avatar")
-    EmployeeResponseDTO EmployeeEntityToEmployeeResponseDTO(EmployeeEntity employeeEntity, String base64Avatar);
+    @Mapping(target = "branchId", source = "employeeEntity.branch.id")
+    @Mapping(target = "servicesIds", source = "employeeEntity.services", qualifiedByName = "mapServicesToIds")
+    EmployeeResponseDTO employeeEntityToEmployeeResponseDTO(EmployeeEntity employeeEntity, String base64Avatar);
+
+    @Named("mapServicesToIds")
+    default List<Long> mapServiceEntitiesToIds(Set<ServiceEntity> employeeEntities) {
+        return employeeEntities.stream()
+                .map(ServiceEntity::getId)
+                .toList();
+    }
 }
