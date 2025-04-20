3457d38b0a67f7e5887827e5a84f0d534c93d5d7
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-20 20:11:31 +0200

    {0.0.37} feat(dto): add existingServicesIds and newServicesDTO fields to EmployeeRequestDTO

--- src/main/java/ua/shpp/dto/EmployeeRequestDTO.java
+++ src/main/java/ua/shpp/dto/EmployeeRequestDTO.java
@@ -1,11 +1,11 @@
 package ua.shpp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import ua.shpp.entity.ServiceEntity;
 
 import java.util.Set;
 
 public record EmployeeRequestDTO(@JsonProperty("branch_id") Long branchId, @JsonProperty("first_name") String firstName,
                                  @JsonProperty("last_name") String lastName, String email, String phone,
-                                 Set<ServiceEntity> services) {
+                                 @JsonProperty("existing_services_ids") Set<Long> existingServicesIds,
+                                 @JsonProperty("new_services") Set<EmployeeServiceCreateDTO> newServicesDTO) {
 }
