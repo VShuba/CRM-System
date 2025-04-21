871e606bb064fc51138180136af63b2e44ebacaa
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-21 17:55:41 +0200

    {0.0.50} feat(service-repo): add findAllServiceNamesByNamesAndBranch method

--- src/main/java/ua/shpp/repository/ServiceRepository.java
+++ src/main/java/ua/shpp/repository/ServiceRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ua.shpp.entity.ServiceEntity;
 
+import java.util.List;
+
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
     @Query("SELECT s.name FROM ServiceEntity s")
     Page<String> findAllServiceNames(Pageable pageable);
+
+    @Query(nativeQuery = true, value = "SELECT service_name FROM services WHERE service_name IN (:names) AND branch_id = :branchId")
+    List<String> findAllServiceNamesByNamesAndBranch(List<String> names, Long branchId);
 }
