9d4013ea993a44e5244852c70444d84f06183e13
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-21 12:45:47 +0200

    {0.0.44} chore(sql): remove manual IDs to prevent breaking auto-increment behavior

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,22 @@
 -- Create user. Password(10 rounds): my_1secret1_password
-INSERT INTO users (id, email, login, password, role)
-VALUES (1, 'test@gmail.com', 'test@gmail.com',
+INSERT INTO users (email, login, password, role)
+VALUES ('test@gmail.com', 'test@gmail.com',
         '$2a$10$vIm9sed1.P4N7dTL.d4SZer2HQYj4Yjlkttb/mOEYhrS4LRfQ5viq', 'ADMIN');
 
 -- Create organization
-INSERT INTO organizations (id, name)
-VALUES (1, 'Test Organization');
+INSERT INTO organizations (name)
+VALUES ('Test Organization');
 
 -- Create branch (location)
-INSERT INTO branches (id, name, organization_id)
-VALUES (1, 'Main Branch', 1);
+INSERT INTO branches (name, organization_id)
+VALUES ('Main Branch', 1);
 
 -- Create rooms
-INSERT INTO rooms (id, name, branch_id)
-VALUES (1, 'Room A', 1),
-       (2, 'Room B', 1);
+INSERT INTO rooms (name, branch_id)
+VALUES ('Room A', 1),
+       ('Room B', 1);
 
 -- Create services
-INSERT INTO services (id, service_name, service_color, branch_id)
-VALUES (1, 'Haircut', '#FF5733', 1),
-       (2, 'Massage', '#33FF57', 1);
+INSERT INTO services (service_name, service_color, branch_id)
+VALUES ('Haircut', '#FF5733', 1),
+       ('Massage', '#33FF57', 1);
