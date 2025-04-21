fb0f8c401334f39758dd3e8b0eb22eda73da1174
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-21 13:48:15 +0200

    {0.0.47} feat(repository): add existsByEmailAndBranchId method

--- src/main/java/ua/shpp/repository/EmployeeRepository.java
+++ src/main/java/ua/shpp/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.shpp.entity.EmployeeEntity;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    boolean existsByEmailAndBranchId(String email, Long branchId);
 }
