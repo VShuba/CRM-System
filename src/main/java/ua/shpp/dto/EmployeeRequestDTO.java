f761997d7e48f3393ac08a7f51fd37942791af15
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-25 22:21:07 +0200

    {0.0.59} fix(dto): replace firstName and lastName with single name field in EmployeeRequestDTO

--- src/main/java/ua/shpp/dto/EmployeeRequestDTO.java
+++ src/main/java/ua/shpp/dto/EmployeeRequestDTO.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Set;
 
-public record EmployeeRequestDTO(@JsonProperty("branch_id") Long branchId, @JsonProperty("first_name") String firstName,
-                                 @JsonProperty("last_name") String lastName, String email, String phone,
+public record EmployeeRequestDTO(@JsonProperty("branch_id") Long branchId, String name, String email, String phone,
                                  @JsonProperty("existing_services_ids") Set<Long> existingServicesIds,
                                  @JsonProperty("new_services") Set<EmployeeServiceCreateDTO> newServicesDTO) {
 }
