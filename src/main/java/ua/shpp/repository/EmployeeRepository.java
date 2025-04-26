88ce20021af8962c4e1fc43b77814ad9e4fb4910
Ihor Afanasiev <t0997833984@gmail.com>
2025-04-26 14:36:00 +0200

    {0.0.60} feat(repository): create deleteEmployeeAndRelatedRecords method

--- src/main/java/ua/shpp/repository/EmployeeRepository.java
+++ src/main/java/ua/shpp/repository/EmployeeRepository.java
@@ -1,8 +1,17 @@
 package ua.shpp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import ua.shpp.entity.EmployeeEntity;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
     boolean existsByEmailAndBranchId(String email, Long branchId);
+
+    @Modifying
+    @Query(nativeQuery = true, value = """
+            DELETE FROM employee_service WHERE employee_id = :id;
+            DELETE FROM employee WHERE id = :id
+            """)
+    int deleteEmployeeAndRelatedRecords(Long id);
 }
