3c5106ff28908b21c97ba7b887a5c1f1df7b334c
Kostaiev <email@mail.com>
2025-04-27 03:48:33 +0300

    Added constructor String EventNotFoundException

--- src/main/java/ua/shpp/exception/EventNotFoundException.java
+++ src/main/java/ua/shpp/exception/EventNotFoundException.java
@@ -4,4 +4,7 @@ public class EventNotFoundException extends RuntimeException {
     public EventNotFoundException(String message) {
         super(message);
     }
+    public EventNotFoundException(Long id) {
+        super("Event not found with id " + id);
+    }
 }
