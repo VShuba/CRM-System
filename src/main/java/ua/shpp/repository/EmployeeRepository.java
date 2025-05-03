6d7a74129ae3531deb6a91dfc09d9e2ebc95f3d5
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-03 17:25:05 +0200

    {0.0.88} chore(employee): remove unused unbindEmployeeFromBranch method

--- src/main/java/ua/shpp/repository/EmployeeRepository.java
+++ src/main/java/ua/shpp/repository/EmployeeRepository.java
@@ -1,18 +1,9 @@
 package ua.shpp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import ua.shpp.entity.EmployeeEntity;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
-    @Modifying
-    @Query(nativeQuery = true, value = """
-            DELETE FROM employee_branch WHERE employee_id = :id AND branch_id = :branchId;
-            DELETE FROM employee_service WHERE employee_id = :id
-                                             AND service_id IN ( SELECT s.id FROM services s WHERE s.branch_id = :branchId );
-            """)
-    int unbindEmployeeFromBranch(Long id, Long branchId);
 
     EmployeeEntity findByEmailAndBranchId(String email, Long branchId);
 }
