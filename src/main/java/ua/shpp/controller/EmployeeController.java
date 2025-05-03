e1e42d224d489b678c7549d516113a2bbddc3708
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-03 22:07:20 +0200

    {0.0.97} fix(controller): fix SpEL enum usage in @PreAuthorize with T(...) syntax

--- src/main/java/ua/shpp/controller/EmployeeController.java
+++ src/main/java/ua/shpp/controller/EmployeeController.java
@@ -53,7 +53,7 @@ public class EmployeeController {
 
     @DeleteMapping("/employee")
     @Operation(summary = "Delete employee from branch")
-    @PreAuthorize("hasAuthority('SUPER_ADMIN') or @authz.hasRoleInOrgByEmployeeId(#requestDTO.employeeId(), OrgRole.MANAGER)")
+    @PreAuthorize("@authz.hasRoleInOrgByEmployeeId(#requestDTO.employeeId(), T(ua.shpp.model.OrgRole).MANAGER)")
     public ResponseEntity<Void> deleteEmployeeFromBranch(@RequestBody EmployeeDeleteRequestDTO requestDTO) {
         if (employeeService.deleteEmployee(requestDTO.employeeId())) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
