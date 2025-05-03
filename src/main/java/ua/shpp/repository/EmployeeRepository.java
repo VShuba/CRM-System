1e3d502f842be9cf90aaeff5343bfcb99f92c459
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-03 19:07:33 +0200

    {0.0.89} fix(branch): restore accidentally deleted method getBranchIdById

--- src/main/java/ua/shpp/repository/EmployeeRepository.java
+++ src/main/java/ua/shpp/repository/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package ua.shpp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ua.shpp.entity.EmployeeEntity;
 
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
 
     EmployeeEntity findByEmailAndBranchId(String email, Long branchId);
+
+    @Query(value = "SELECT branch_id FROM employee WHERE id = :employeeId", nativeQuery = true)
+    Long getBranchIdById(Long employeeId);
 }
