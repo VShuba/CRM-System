6a78e14b55efe41b7617036be69807c56f8d0521
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-03 20:47:05 +0200

    {0.0.93} feat(repository): add getUserRoleInOrganization method

--- src/main/java/ua/shpp/repository/UserOrganizationRepository.java
+++ src/main/java/ua/shpp/repository/UserOrganizationRepository.java
@@ -1,6 +1,7 @@
 package ua.shpp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ua.shpp.entity.UserOrganization;
 import ua.shpp.entity.UserOrganizationId;
@@ -9,4 +10,11 @@ import ua.shpp.model.OrgRole;
 @Repository
 public interface UserOrganizationRepository extends JpaRepository<UserOrganization, UserOrganizationId> {
     boolean existsByUserIdAndOrganizationIdAndRole(Long userId, Long organizationId, OrgRole role);
+
+    @Query(value = """
+            SELECT organization_role
+            FROM user_organization
+            WHERE user_id = :userId AND organization_id = :organizationId
+            """, nativeQuery = true)
+    OrgRole getUserRoleInOrganization(Long userId, Long organizationId);
 }
