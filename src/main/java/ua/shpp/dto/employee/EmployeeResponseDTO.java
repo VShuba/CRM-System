ae8a2f42b10870335bc930fea9438d471455f0da
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-04 09:49:04 +0200

    {0.0.101} refactor(dto): remove serviceIds field from EmployeeResponseDTO

--- src/main/java/ua/shpp/dto/employee/EmployeeResponseDTO.java
+++ src/main/java/ua/shpp/dto/employee/EmployeeResponseDTO.java
@@ -2,11 +2,8 @@ package ua.shpp.dto.employee;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 public record EmployeeResponseDTO(@JsonProperty("employee_id") Long id,
                                   String name, String email, String phone,
                                   @JsonProperty("branch_id") Long branchId,
-                                  @JsonProperty("service_ids") List<Long> serviceIds,
                                   @JsonProperty("base64_avatar") String base64Avatar) {
 }
