09e080e420ca69b98d3922bae0de1ab62be86ca6
Ihor Afanasiev <t0997833984@gmail.com>
2025-05-05 17:27:29 +0200

    {0.0.110} feat(dto): add services field

--- src/main/java/ua/shpp/dto/employee/EmployeeResponseDTO.java
+++ src/main/java/ua/shpp/dto/employee/EmployeeResponseDTO.java
@@ -2,8 +2,11 @@ package ua.shpp.dto.employee;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record EmployeeResponseDTO(@JsonProperty("employee_id") Long id,
                                   String name, String email, String phone,
                                   @JsonProperty("branch_id") Long branchId,
+                                  List<EmployeeServicesResponseDTO> services,
                                   @JsonProperty("base64_avatar") String base64Avatar) {
 }
