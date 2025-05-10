7e0bc1678a3ecb4d19453249e77d2561f1ca7af8
Andrii Zaika <lytiy7404@gmail.com>
2025-05-10 13:48:03 +0300

    add first mapping for client from tsv

--- src/main/java/ua/shpp/controller/ImportClientsController.java
+++ src/main/java/ua/shpp/controller/ImportClientsController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.shpp.dto.ClientRequestDto;
 import ua.shpp.service.ImportClientsService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -16,7 +19,7 @@ public class ImportClientsController {
     private final ImportClientsService service;
 
     @PostMapping("/organizations/{orgId}/clients/import")
-    public ResponseEntity<String> importClientsFromSheet(@RequestBody String sheetId) {
+    public ResponseEntity<List<ClientRequestDto>> importClientsFromSheet(@RequestBody String sheetId) {
         return ResponseEntity.ok(service.importClients(sheetId));
     }
 }
--- src/main/java/ua/shpp/service/ImportClientsService.java
+++ src/main/java/ua/shpp/service/ImportClientsService.java
@@ -1,22 +1,36 @@
 package ua.shpp.service;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
+import ua.shpp.dto.ClientRequestDto;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class ImportClientsService {
 
+    private final CsvMapper csvMapper;
+
+    //todo розібратися restclient - webclient
     private final WebClient webClient = WebClient.builder()
             .clientConnector(new ReactorClientHttpConnector(
                     HttpClient.create().followRedirect(true)
             ))
             .build();
 
-    public String importClients(String sheetId) {
+    public List<ClientRequestDto> importClients(String sheetId) {
         String initialUrl = "https://docs.google.com/spreadsheets/d/" + sheetId + "/export?format=tsv&gid=0";
         log.info("ImportClientsService uri: {}", initialUrl);
 
@@ -27,6 +41,36 @@ public class ImportClientsService {
                 .block();
 
         log.info("Content:\n{}", content);
-        return content;
+        return parseTsvWithJackson(content);
+    }
+
+
+    //todo додати хендл всіх форматів дат( або хоча б більшості)
+    private List<ClientRequestDto> parseTsvWithJackson(String tsvContet) {
+        List<ClientRequestDto> validClients = new ArrayList<>();
+        CsvSchema csvSchema = CsvSchema.builder()
+                .setColumnSeparator('\t')
+                .setUseHeader(true)
+                .build();
+            try (MappingIterator<ClientRequestDto> iterator = csvMapper.readerFor(ClientRequestDto.class)
+                    .with(csvSchema)
+                    .readValues(tsvContet)) {
+                while (iterator.hasNext()) {
+                    ClientRequestDto clientRequestDto = iterator.next();
+
+                    if (isValid(clientRequestDto)){
+                        validClients.add(clientRequestDto);
+                    } else {
+                        log.warn("Invalid client data: {}", clientRequestDto);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        return validClients;
+    }
+
+    private boolean isValid(@Valid ClientRequestDto clientRequestDto) {
+        return clientRequestDto != null;
     }
 }
